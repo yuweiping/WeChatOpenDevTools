5abb43e23f10a2cda87b91d148474b8fe81002ba
yuweiping <yuweiping@yeah.net>
2024-01-05 12:10:43 +0800

    init

---
+++ README.md
@@ -0,0 +1,31 @@
+# WeChatOpenDevTools
+
+### 介绍
+
+基于志远老师的WeChatOpenDevTools，用python来实现的。
+因为我在win11下node-gyp编译失败，win10环境可以正常用。折腾好几天猜测是因为VS环境的win sdk不匹配。
+
+本项目只实现了其中一种，目前够用，没计划实现另一种。
+
+参考链接
+
+https://github.com/shuaibibobo/WeChatOpenDevTools
+
+https://github.com/x0tools/WeChatOpenDevTools
+
+[【志远】[公开课\] xcx某信X64DBG自更新教程_哔哩哔哩_bilibili](https://www.bilibili.com/video/BV1Cw411473D/?spm_id_from=333.999.0.0)
+
+---
+
+
+
+
+这是一个专门为爬虫领域制作的库,用来快速调试PC端的微信浏览器和微信小程序.原理是利用Frida Hook微信的配置项,并使用反汇编技术修改了微信的指令集。
+
+**注意只支持 Window 平台！！！！**
+
+注意本库只能作为学习用途, 造成的任何问题与本库开发者无关, 如侵犯到你的权益，请联系删除
+
+
+
+**需要提前运行微信.需要提前运行微信.需要提前运行微信**
---
+++ main.py
@@ -0,0 +1,70 @@
+import sys
+
+import frida
+import psutil
+import pathlib
+
+
+def get_pid():
+    process_name = "WeChatAppEx.exe"
+    device = frida.get_local_device()
+    processes = device.enumerate_processes()
+    for process in processes:
+        if process.name == process_name:
+            cmdline = psutil.Process(process.pid).cmdline()
+            # print(cmdline)
+            if "--type=" not in cmdline:
+                if not any('--type=' in arg for arg in cmdline):
+                    print(process.pid)
+                    return process.pid
+
+
+def on_message(message, data):
+    if message['type'] == 'send':
+        print(message['payload'])
+    elif message['type'] == 'error':
+        print(message['stack'])
+
+
+def load_address_source(version, bit):
+    address_source = ""
+    address_source_head_file_path = pathlib.Path(__file__).parent / f"Core/AddressSource.head"
+    address_source_end_file_path = pathlib.Path(__file__).parent / f"Core/AddressSource.end"
+    address_file_path = pathlib.Path(__file__).parent / f"Core/WeChatAppEx.exe/address_{version}_{bit}.json"
+    hook_file_path = pathlib.Path(__file__).parent / f"Core/WeChatAppEx.exe/hook.js"
+
+    try:
+        assert address_file_path.exists(), f"File {address_file_path} does not exist"
+        with open(address_source_head_file_path, 'r', encoding='utf-8') as f:
+            address_source += f.read()
+        with open(address_file_path, 'r', encoding='utf-8') as f:
+            address_source += f.read()
+        with open(address_source_end_file_path, 'r', encoding='utf-8') as f:
+            address_source += f.read()
+        with open(hook_file_path, 'r', encoding='utf-8') as f:
+            address_source += f.read()
+        print("HOOK文件组装成功!")
+        return address_source
+    except AssertionError as e:
+        print(f"发生错误: {str(e)}")
+        return None
+    except Exception as e:
+        print(f"发生错误: {str(e)}")
+        return None
+
+
+if __name__ == '__main__':
+    pid = get_pid()
+    version = input("请输入版本号 (例如: 8531): ")
+    session = frida.attach(pid)
+    address = load_address_source(version,'x64')
+    if address is not None:
+        with open('./address.js', 'r', encoding='utf-8') as file:
+            address_source = file.read()
+
+        # print(address_source)
+        script = session.create_script(address_source)
+        script.on('message', on_message)
+        script.load()
+        sys.stdin.read()
+        session.detach()
